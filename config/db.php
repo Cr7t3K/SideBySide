d1fe533b71972c9cb405522d8831cf65a5571d14
Cr7t3K <cvr.contact.pro@gmail.com>
2020-06-11 18:52:01 +0200

    test

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file define BD access infos.
+ *
+ * PHP version 7
+ *
+ * @category  PHP
+ * @package   PHP_SimpleMVC
+ * @author    WCS <contact@wildcodeschool.fr>
+ * @license  --
+ * @link     https://github.com/WildCodeSchool/simple-mvc
+ *
+ */
+
+define('APP_DB_USER', 'b32b22d815651a');
+define('APP_DB_PWD', '8aa0ea0f');
+define('APP_DB_HOST', 'eu-cdbr-west-03.cleardb.net');
+define('APP_DB_NAME', 'heroku_337e349bd1f4476');
